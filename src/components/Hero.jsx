43b3b80c75d1008df89d86d2d02077c1248bee83
blaztted <bbc.silva@gmail.com>
2024-03-12 21:08:54 +0000

    created hero section with animated photo grid

---
+++ src/components/Hero.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+//import photo-grid from "../assets/img/photo-grid.jpg";
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="infinite-photo-grid"></div>
+      <h1>Online Experiences</h1>
+      <p>
+        Join unique interactive activities led by one-of-a-kind hosts—all
+        without leaving home.
+      </p>
+    </div>
+  );
+};
+
+export default Hero;
