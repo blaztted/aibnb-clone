194d37cf1144a62c429a1989edff4a7327f9d7a2
blaztted <bbc.silva@gmail.com>
2024-03-12 21:33:32 +0000

    styled navbar and hero section in order to dont interfere with each other

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import logo from "../assets/img/airbnb-logo.png";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Navbar() {
+  const [scrolling, setScrolling] = useState(false);
+
+  // Listen for scrolling event
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setScrolling(true);
+      } else {
+        setScrolling(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <nav>
+    <nav className={scrolling ? "nav-scrolled" : ""}>
       <img src={logo} className="nav--logo" />
     </nav>
   );
